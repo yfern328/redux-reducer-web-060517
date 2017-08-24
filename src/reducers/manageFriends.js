33b90ff36a6821b5b10d8b27b315aa14f70e3cac
yfern328 <yfernan2@u.rochester.edu>
2017-08-24 12:55:29 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND' :
+      return {friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND' :
+      return {friends: state.friends.filter(friend => {return friend.id !== action.id})}
+    default :
+      return state;
+  }
 }
